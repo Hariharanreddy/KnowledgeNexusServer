94309f13def9a7aa334d1ea0518e96da3d12376f
hariharan <animatrix535@gmail.com>
2023-04-12 11:54:33 +0530

    changed config

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ cloudinary.v2.config({
   api_secret: process.env.CLOUDINARY_CLIENT_SECRET,
 });
 
-export const instance = new RazorPay({
+export const instance = new RazorPay({ 
   key_id: process.env.RAZORPAY_API_KEY,
   key_secret: process.env.RAZORPAY_API_SECRET,
 });
